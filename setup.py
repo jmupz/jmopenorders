ad9ced59fb5ebe5b2e2c25b71fd33891f9c1c1eb
Jürgen Mülbert <juergen.muelbert@gmail.com>
2019-12-18 23:06:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -116,7 +116,6 @@ setup(
     install_requirements=REQUIREMENTS,
     package_dir={'': 'src'},
     packages=find_packages(
-        'jmopenorders',
         exclude=["contrib", "docs", "tests*", "tasks"],
     ),
     entry_points={
