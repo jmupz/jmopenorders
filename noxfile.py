ec9b71313f1842d5f9096626c410a3d4e083f77c
Jürgen Mülbert <juergen.muelbert@gmail.com>
2020-06-28 12:42:34 +0200

    Add necessary package to noxfile.

--- noxfile.py
+++ noxfile.py
@@ -141,7 +141,7 @@ def mypy(session: Session) -> None:
 def tests(session: Session) -> None:
     """Run the test suite."""
     install_package(session)
-    install(session, "coverage[toml]", "pytest")
+    install(session, "coverage[toml]", "pytest", "faker")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
     finally:
